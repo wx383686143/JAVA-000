4935212b116382f860298af852dc56fdec89b9f4
wx383686143 <wx383686143@126.com>
2020-10-25 18:01:55 +0800

    关闭流

--- Week_02/HelloHttpClient.java
+++ Week_02/HelloHttpClient.java
@@ -1,5 +1,5 @@
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -10,19 +10,44 @@ import java.io.InputStream;
 public class HelloHttpClient {
 
     public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        CloseableHttpResponse response = null;
+        InputStream in = null;
         try {
-            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
             HttpGet httpGet = new HttpGet("http://localhost:8801");
-            HttpResponse response = httpClient.execute(httpGet);
+            response = httpClient.execute(httpGet);
             HttpEntity entity = response.getEntity();
             if (null != entity) {
-                InputStream in = entity.getContent();
+                in = entity.getContent();
                 byte[] bytes = new byte[in.available()];
                 in.read(bytes);
                 System.out.println(new String(bytes));
             }
+
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if(null != in) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(null != httpClient) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(null != in) {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
